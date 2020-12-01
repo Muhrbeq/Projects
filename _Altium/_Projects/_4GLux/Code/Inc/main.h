b59c694adebfe887d11ba15145c8b80726cef559
Rasmus Muhrbeck <Rasmus.Muhrbeck@exeger.com>
2020-12-01 13:43:48 +0100

    updated with functioning code

---
+++ _Altium/_Projects/_4GLux/Code/Inc/main.h
@@ -0,0 +1,133 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_ll_usart.h"
+#include "stm32l4xx_ll_rcc.h"
+#include "stm32l4xx.h"
+#include "stm32l4xx_ll_system.h"
+#include "stm32l4xx_ll_gpio.h"
+#include "stm32l4xx_ll_exti.h"
+#include "stm32l4xx_ll_bus.h"
+#include "stm32l4xx_ll_cortex.h"
+#include "stm32l4xx_ll_utils.h"
+#include "stm32l4xx_ll_pwr.h"
+#include "stm32l4xx_ll_dma.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define Spectrum_Enable_Pin LL_GPIO_PIN_13
+#define Spectrum_Enable_GPIO_Port GPIOC
+#define VBat_Sense_Pin LL_GPIO_PIN_0
+#define VBat_Sense_GPIO_Port GPIOC
+#define VBat_Sense_Enable_Pin LL_GPIO_PIN_1
+#define VBat_Sense_Enable_GPIO_Port GPIOC
+#define Revision_Enable_Pin LL_GPIO_PIN_2
+#define Revision_Enable_GPIO_Port GPIOC
+#define ADC_Revision_Pin LL_GPIO_PIN_3
+#define ADC_Revision_GPIO_Port GPIOC
+#define SendData_EXTI0_Pin LL_GPIO_PIN_0
+#define SendData_EXTI0_GPIO_Port GPIOA
+#define Radio_NetStatus_Pin LL_GPIO_PIN_1
+#define Radio_NetStatus_GPIO_Port GPIOA
+#define Radio_Enable_Pin LL_GPIO_PIN_4
+#define Radio_Enable_GPIO_Port GPIOA
+#define Sara_Power_On_Pin LL_GPIO_PIN_5
+#define Sara_Power_On_GPIO_Port GPIOA
+#define Sara_Reset_Pin LL_GPIO_PIN_6
+#define Sara_Reset_GPIO_Port GPIOA
+#define SD_Detect_Pin LL_GPIO_PIN_7
+#define SD_Detect_GPIO_Port GPIOA
+#define GPS_Enable_Pin LL_GPIO_PIN_0
+#define GPS_Enable_GPIO_Port GPIOB
+#define Screen_Enable_Pin LL_GPIO_PIN_1
+#define Screen_Enable_GPIO_Port GPIOB
+#define Acceleromater_Enable_Pin LL_GPIO_PIN_2
+#define Acceleromater_Enable_GPIO_Port GPIOB
+#define SPI_CS_Pin LL_GPIO_PIN_12
+#define SPI_CS_GPIO_Port GPIOB
+#define Screen_DC_Pin LL_GPIO_PIN_6
+#define Screen_DC_GPIO_Port GPIOC
+#define Screen_RST_Pin LL_GPIO_PIN_7
+#define Screen_RST_GPIO_Port GPIOC
+#define Screen_Busy_Pin LL_GPIO_PIN_8
+#define Screen_Busy_GPIO_Port GPIOA
+#define USART1_TX_PC_Pin LL_GPIO_PIN_9
+#define USART1_TX_PC_GPIO_Port GPIOA
+#define USART1_RX_PC_Pin LL_GPIO_PIN_10
+#define USART1_RX_PC_GPIO_Port GPIOA
+#define LED_Green_Pin LL_GPIO_PIN_11
+#define LED_Green_GPIO_Port GPIOA
+#define LED_Red_Pin LL_GPIO_PIN_12
+#define LED_Red_GPIO_Port GPIOA
+#define SD_Enable_Pin LL_GPIO_PIN_15
+#define SD_Enable_GPIO_Port GPIOA
+#define VBUS_Detect_EXTI4_Pin LL_GPIO_PIN_4
+#define VBUS_Detect_EXTI4_GPIO_Port GPIOB
+#define Sensor_Enable_Pin LL_GPIO_PIN_5
+#define Sensor_Enable_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ _Altium/_Projects/_4GLux/Code/Src/gpio.c
@@ -0,0 +1,146 @@
+/**
+  ******************************************************************************
+  * File Name          : gpio.c
+  * Description        : This file provides code for the configuration
+  *                      of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+        * Free pins are configured automatically as Analog (this feature is enabled through 
+        * the Code Generation settings)
+*/
+void MX_GPIO_Init(void)
+{
+
+  LL_EXTI_InitTypeDef EXTI_InitStruct = {0};
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOC);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOH);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOD);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOC, Spectrum_Enable_Pin|VBat_Sense_Enable_Pin|Revision_Enable_Pin|Screen_DC_Pin 
+                          |Screen_RST_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOA, Radio_Enable_Pin|Sara_Power_On_Pin|Sara_Reset_Pin|Screen_Busy_Pin 
+                          |LED_Green_Pin|LED_Red_Pin|SD_Enable_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOB, GPS_Enable_Pin|Screen_Enable_Pin|Acceleromater_Enable_Pin|SPI_CS_Pin 
+                          |Sensor_Enable_Pin);
+
+  /**/
+  GPIO_InitStruct.Pin = Spectrum_Enable_Pin|VBat_Sense_Enable_Pin|Revision_Enable_Pin|Screen_DC_Pin 
+                          |Screen_RST_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = Radio_NetStatus_Pin|SD_Detect_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = Radio_Enable_Pin|Sara_Power_On_Pin|Sara_Reset_Pin|Screen_Busy_Pin 
+                          |LED_Green_Pin|LED_Red_Pin|SD_Enable_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = GPS_Enable_Pin|Screen_Enable_Pin|Acceleromater_Enable_Pin|SPI_CS_Pin 
+                          |Sensor_Enable_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_3;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
+  /**/
+  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTA, LL_SYSCFG_EXTI_LINE0);
+
+  /**/
+  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE4);
+
+  /**/
+  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_0;
+  EXTI_InitStruct.Line_32_63 = LL_EXTI_LINE_NONE;
+  EXTI_InitStruct.LineCommand = ENABLE;
+  EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_FALLING;
+  LL_EXTI_Init(&EXTI_InitStruct);
+
+  /**/
+  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_4;
+  EXTI_InitStruct.Line_32_63 = LL_EXTI_LINE_NONE;
+  EXTI_InitStruct.LineCommand = ENABLE;
+  EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
+  LL_EXTI_Init(&EXTI_InitStruct);
+
+  /**/
+  LL_GPIO_SetPinPull(SendData_EXTI0_GPIO_Port, SendData_EXTI0_Pin, LL_GPIO_PULL_NO);
+
+  /**/
+  LL_GPIO_SetPinPull(VBUS_Detect_EXTI4_GPIO_Port, VBUS_Detect_EXTI4_Pin, LL_GPIO_PULL_NO);
+
+  /**/
+  LL_GPIO_SetPinMode(SendData_EXTI0_GPIO_Port, SendData_EXTI0_Pin, LL_GPIO_MODE_INPUT);
+
+  /**/
+  LL_GPIO_SetPinMode(VBUS_Detect_EXTI4_GPIO_Port, VBUS_Detect_EXTI4_Pin, LL_GPIO_MODE_INPUT);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
