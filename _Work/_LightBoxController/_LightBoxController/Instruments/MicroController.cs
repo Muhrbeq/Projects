3c6a916c436c97edca250d2a1728d4433e2f4783
Muhrbeq <Rasmus.muhrbeck@gmail.com>
2021-04-20 07:10:28 +0200

    Foldable menu

---
+++ _Work/_LightBoxController/_LightBoxController/Instruments/MicroController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _LightBoxController.Instruments
+{
+    public class MicroController
+    {
+        /* Can be whatever Microcontroller, Needs to be connected via UART */
+        public string Role;
+        
+    }
+
+    public enum MicroControllerRole
+    {
+        DEFAULT,
+        TEMPERATURE,
+        HUMIDITY,
+        CELLMEASUREMENT,
+        ERROR,
+    }
+
+    public enum MicroControllerStatus
+    {
+        IDLE,
+        STOPPED,
+        SAMPLING,
+        ERROR,
+
+    }
+}
